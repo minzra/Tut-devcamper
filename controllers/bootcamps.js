b780c82250d92d83680de93b4076686de72c0166
minzra <zaraispahany1@gmail.com>
2020-12-25 23:26:41 +1100

    16.1 Intro to Middleware - explanation and simple example

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,7 +3,15 @@
 // @access  Public
 exports.getBootcamps = (req, res, next) => {
   // got this from routes/bootcamps.js
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+  res
+    .status(200)
+    .json({ success: true, msg: 'Show all bootcamps', hello: req.hello });
+  // we have access to the middleware we created in server.js ^
+  // and this will show the hello world within postman
+  // when we build our authentication middleware, we are going to validate a token that's sent in
+  // and if that token validates then we are going to set a user on this so like req.user
+  // and that user will come from the db and that will be the current loggedin user.
+  // Then we can use that within our methods here to do certain things
 };
 
 // @desc    Get single bootcamps
--- server.js
+++ server.js
@@ -10,6 +10,19 @@ dotenv.config({ path: './config/config.env' });
 // Initialize app variable with express
 const app = express();
 
+// - Middleware is a function that has acces to the request/response cycle and runs during that cycle and you can set request variables.
+// so any request we make this function will run
+const logger = (req, res, next) => {
+  // set a value on this request object and we can then access from
+  // any route after this middleware
+  req.hello = 'hello world';
+  console.log('Middleware ran');
+  next(); // You need to call this so it knows to move onto the next piece of middleware cycle
+};
+
+// to use the middleware we run app.use
+app.use(logger);
+
 // Mount routers from line 4
 app.use('/api/v1/bootcamps', bootcamps);
 
